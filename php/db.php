44f05bd47cbf4bcf0b2659909ddc58206c2cb7e8
Aytixel <36609903+Aytixel@users.noreply.github.com>
2019-10-11 19:37:54 +0200

    Update db.php

--- php/db.php
+++ php/db.php
@@ -1,4 +1,4 @@
 <?php
-$pdo = new PDO('mysql:dbname=a3407628_member;host=mysql6.000webhost.com', 'a3407628_root', 'uploader1');
+$pdo = new PDO('mysql:dbname=dbname;host=host', 'user', 'password');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
